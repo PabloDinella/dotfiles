18dd00d83646bf3a138d690328f393f8a162a630
Pablo R. Dinella <pablo.dinella@gmail.com>
2018-10-31 18:46:22 -0300

    Add nvm instalation script

--- ubuntu_post_install.sh
+++ ubuntu_post_install.sh
@@ -25,3 +25,9 @@ mv PowerlineSymbols.otf ~/.local/share/fonts/
 fc-cache -vf ~/.local/share/fonts/
 mkdir -p ~/.config/fontconfig/conf.d/
 mv 10-powerline-symbols.conf ~/.config/fontconfig/conf.d/
+
+# install nvm
+curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.11/install.sh | bash
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
