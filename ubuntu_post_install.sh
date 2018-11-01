e0235fbb1898c1af393eed144fdfcce40b9ef5d1
Pablo R. Dinella <pablo.dinella@gmail.com>
2018-11-01 08:36:11 -0300

    add vim installation

--- ubuntu_post_install.sh
+++ ubuntu_post_install.sh
@@ -1,6 +1,9 @@
 sudo apt update -y && sudo apt upgrade -y
 
-sudo apt install git zsh -y
+sudo apt install -y \
+git \
+zsh \
+vim
 
 # install chrome
 sudo wget -O - https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb > /tmp/chrome.deb
