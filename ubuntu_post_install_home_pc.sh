34042348d581da6f64da3a4afff72b8652c25273
Pablo R. Dinella <pablo.dinella@gmail.com>
2018-12-08 21:18:25 -0200

    Adding audacity, inkscape and gimp to ubuntu_post_install_home_pc.sh

--- ubuntu_post_install_home_pc.sh
+++ ubuntu_post_install_home_pc.sh
@@ -2,7 +2,10 @@ sudo apt update -y && sudo apt upgrade -y
 
 sudo apt install -y \
 obs-studio \
-kdenlive
+kdenlive \ 
+audacity \
+inkscape \
+gimp
 
 sudo add-apt-repository multiverse
 sudo apt install steam
