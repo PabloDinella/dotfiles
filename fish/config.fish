ca69e9932747ac8336691894d31dc1d4e95102e7
PabloDinella <pablo.dinella@gmail.com>
2020-04-04 11:50:17 -0300

    update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -1,15 +1,15 @@
-abbr gst='git status'
-abbr gd='git diff'
-abbr gl='git pull'
-abbr glo='git pull origin'
-abbr gp='git push'
-abbr gpo='git push origin'
-abbr gcmsg='git commit -m'
-abbr gco='git checkout'
-abbr gcm='git checkout master'
-abbr glg='git log --stat --max-count=10'
-abbr ga='git add'
-abbr gaa='git add .'
-abbr gsta='git stash'
-abbr gstaa='git stash apply'
+abbr --add gst git status
+abbr --add gd git diff
+abbr --add gl git pull
+abbr --add glo git pull origin
+abbr --add gp git push
+abbr --add gpo git push origin
+abbr --add gcmsg git commit -m
+abbr --add gco git checkout
+abbr --add gcm git checkout master
+abbr --add glg git log --stat --max-count=10
+abbr --add ga git add
+abbr --add gaa git add .
+abbr --add gsta git stash
+abbr --add gstaa git stash apply
 
