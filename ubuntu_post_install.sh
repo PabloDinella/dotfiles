798667c5a9585c5351475cc678b2cdece06e3ff5
Pablo R. Dinella <pablo.dinella@gmail.com>
2020-06-09 18:42:56 -0300

    update docker-compose

--- ubuntu_post_install.sh
+++ ubuntu_post_install.sh
@@ -94,6 +94,6 @@ sudo usermod -aG docker $USER
 
 # docker-compose installation
 # https://docs.docker.com/compose/install/
-sudo curl -L "https://github.com/docker/compose/releases/download/1.23.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
