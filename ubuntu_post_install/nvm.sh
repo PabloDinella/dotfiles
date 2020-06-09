a255de3634323e96590c95a3af3d10970a727aaf
Pablo R. Dinella <pablo.dinella@gmail.com>
2020-06-09 18:37:03 -0300

    Create nvm.sh

---
+++ ubuntu_post_install/nvm.sh
@@ -0,0 +1,6 @@
+# official nvm installation https://github.com/nvm-sh/nvm#install--update-script
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
+
+# fish integration https://github.com/FabioAntunes/fish-nvm#install
+fisher add FabioAntunes/fish-nvm
+fisher add edc/bass
