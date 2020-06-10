f02d30adad096199015bbbb51a64474a4f7ebf18
PabloDinella <pablo.dinella@gmail.com>
2020-06-09 23:50:10 -0300

    add flameshot and shortcut

--- ubuntu_post_install.sh
+++ ubuntu_post_install.sh
@@ -96,3 +96,5 @@ newgrp docker
 sudo curl -L "https://github.com/docker/compose/releases/download/1.26.0/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
+python3 setCustomShortcut.py 'flameshot gui' 'flameshot gui' '<Control><Alt>equal'
+
